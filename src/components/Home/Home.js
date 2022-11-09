cd213c831f8d8c5e83150935ebac76e46a67d5e8
Saifuddinmonna <Saifuddinmonna@gmail.com>
2022-11-10 03:41:00 +0600

    home page modification addd

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -25,6 +25,31 @@ const Home = () => {
 				))}
 			</div>
 
+			<div
+				className="hero min-h-screen"
+				style={{
+					backgroundImage: `url("https://placeimg.com/1000/800/arch")`,
+				}}>
+				<div className="hero-overlay bg-opacity-60  mx-3 p-3 border- rounded-lg"></div>
+				<div className="hero-content text-center text-neutral-content">
+					<div className="max-w-md mx-3 p-3 border- rounded-lg">
+						<h1 className="mb-5 text-5xl font-bold">ABOUT ME</h1>
+						<p className="mb-5">
+							I am here to get electronics up and running, and
+							keep it that way. When our skilled techs are
+							finished working on your device, it’ll work. Period.
+							We’re pros when it comes to fixing laptops and
+							phones but we can also recover your data. We also
+							repair all types of iDevices. Your iPhone, iPad or
+							iPod are the “Apple” of our eye. Do you have another
+							piece of electronic equipment that is broken and
+							needs special attention? Yeah, we fix that.
+						</p>
+						<button className="btn btn-primary">Get Started</button>
+					</div>
+				</div>
+			</div>
+
 			<Outlet></Outlet>
 		</div>
 	);
